5fed96ed4e7516ae62bd77ddb8eca37354538266
Sitthata <aprils96950@gmail.com>
2024-11-03 14:24:08 +0700

    Add status attribute to articles and comments with validations

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,6 +1,13 @@
 class Article < ApplicationRecord
+  has_many :comments
   validates :title, presence: true
   validates :body, presence: true, length: { minimum: 10 }
 
-  has_many :comments
+  VALID_STATUSES = [ "public", "private", "archived" ]
+
+  validates :status, inclusion: { in: VALID_STATUSES }
+
+  def archived?
+    status == "archived"
+  end
 end
